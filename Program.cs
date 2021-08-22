1605d5cecadd76538bf5980d040ff5e381d64e79
NikulovAlexey <88848186+NikulovAlexey@users.noreply.github.com>
2021-08-22 17:50:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Алексей
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
